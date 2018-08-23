c2d4988266986fa80a870574732bac59ace46c9e
joonatan@work <joonatanrenel@gmail.com>
2018-08-23 20:18:04 +0300

    Updated interval for motor control component numericupDown -> Set to 10.

--- GUI/StepperMotorGUI/StepperMotorGUI/MotorControl.Designer.cs
+++ GUI/StepperMotorGUI/StepperMotorGUI/MotorControl.Designer.cs
@@ -31,39 +31,43 @@
             this.numericUpDownSpeed = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonOne32 = new System.Windows.Forms.RadioButton();
+            this.radioButtonOne16 = new System.Windows.Forms.RadioButton();
+            this.radioButtonQuarterStep = new System.Windows.Forms.RadioButton();
+            this.radioButtonOne8 = new System.Windows.Forms.RadioButton();
             this.radioButtonHalfStep = new System.Windows.Forms.RadioButton();
             this.radioButtonFullStep = new System.Windows.Forms.RadioButton();
             this.label2 = new System.Windows.Forms.Label();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.radioButtonOne8 = new System.Windows.Forms.RadioButton();
-            this.radioButtonQuarterStep = new System.Windows.Forms.RadioButton();
-            this.radioButtonOne16 = new System.Windows.Forms.RadioButton();
-            this.radioButtonOne32 = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSpeed)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // numericUpDownSpeed
             // 
-            this.numericUpDownSpeed.Location = new System.Drawing.Point(129, 17);
-            this.numericUpDownSpeed.Margin = new System.Windows.Forms.Padding(2);
+            this.numericUpDownSpeed.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownSpeed.Location = new System.Drawing.Point(172, 21);
+            this.numericUpDownSpeed.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.numericUpDownSpeed.Maximum = new decimal(new int[] {
             240,
             0,
             0,
             0});
             this.numericUpDownSpeed.Name = "numericUpDownSpeed";
-            this.numericUpDownSpeed.Size = new System.Drawing.Size(90, 20);
+            this.numericUpDownSpeed.Size = new System.Drawing.Size(120, 22);
             this.numericUpDownSpeed.TabIndex = 0;
             this.numericUpDownSpeed.ValueChanged += new System.EventHandler(this.numericUpDownSpeed_ValueChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 18);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(23, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.Size = new System.Drawing.Size(131, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Motor speed (RPM)";
             // 
@@ -78,22 +82,69 @@
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.numericUpDownSpeed);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(2, -1);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Location = new System.Drawing.Point(3, -1);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Size = new System.Drawing.Size(231, 157);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(308, 193);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
+            // radioButtonOne32
+            // 
+            this.radioButtonOne32.AutoSize = true;
+            this.radioButtonOne32.Checked = true;
+            this.radioButtonOne32.Location = new System.Drawing.Point(172, 164);
+            this.radioButtonOne32.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButtonOne32.Name = "radioButtonOne32";
+            this.radioButtonOne32.Size = new System.Drawing.Size(88, 21);
+            this.radioButtonOne32.TabIndex = 8;
+            this.radioButtonOne32.TabStop = true;
+            this.radioButtonOne32.Text = "1/32 step";
+            this.radioButtonOne32.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonOne16
+            // 
+            this.radioButtonOne16.AutoSize = true;
+            this.radioButtonOne16.Location = new System.Drawing.Point(172, 142);
+            this.radioButtonOne16.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButtonOne16.Name = "radioButtonOne16";
+            this.radioButtonOne16.Size = new System.Drawing.Size(88, 21);
+            this.radioButtonOne16.TabIndex = 7;
+            this.radioButtonOne16.Text = "1/16 step";
+            this.radioButtonOne16.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonQuarterStep
+            // 
+            this.radioButtonQuarterStep.AutoSize = true;
+            this.radioButtonQuarterStep.Location = new System.Drawing.Point(172, 97);
+            this.radioButtonQuarterStep.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButtonQuarterStep.Name = "radioButtonQuarterStep";
+            this.radioButtonQuarterStep.Size = new System.Drawing.Size(80, 21);
+            this.radioButtonQuarterStep.TabIndex = 6;
+            this.radioButtonQuarterStep.Text = "1/4 step";
+            this.radioButtonQuarterStep.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonOne8
+            // 
+            this.radioButtonOne8.AutoSize = true;
+            this.radioButtonOne8.Location = new System.Drawing.Point(172, 119);
+            this.radioButtonOne8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButtonOne8.Name = "radioButtonOne8";
+            this.radioButtonOne8.Size = new System.Drawing.Size(80, 21);
+            this.radioButtonOne8.TabIndex = 5;
+            this.radioButtonOne8.Text = "1/8 step";
+            this.radioButtonOne8.UseVisualStyleBackColor = true;
+            // 
             // radioButtonHalfStep
             // 
             this.radioButtonHalfStep.AutoSize = true;
-            this.radioButtonHalfStep.Location = new System.Drawing.Point(129, 61);
+            this.radioButtonHalfStep.Location = new System.Drawing.Point(172, 75);
+            this.radioButtonHalfStep.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButtonHalfStep.Name = "radioButtonHalfStep";
-            this.radioButtonHalfStep.Size = new System.Drawing.Size(65, 17);
+            this.radioButtonHalfStep.Size = new System.Drawing.Size(80, 21);
             this.radioButtonHalfStep.TabIndex = 4;
             this.radioButtonHalfStep.Text = "1/2 step";
             this.radioButtonHalfStep.UseVisualStyleBackColor = true;
@@ -101,9 +152,10 @@
             // radioButtonFullStep
             // 
             this.radioButtonFullStep.AutoSize = true;
-            this.radioButtonFullStep.Location = new System.Drawing.Point(129, 43);
+            this.radioButtonFullStep.Location = new System.Drawing.Point(172, 53);
+            this.radioButtonFullStep.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButtonFullStep.Name = "radioButtonFullStep";
-            this.radioButtonFullStep.Size = new System.Drawing.Size(66, 17);
+            this.radioButtonFullStep.Size = new System.Drawing.Size(84, 21);
             this.radioButtonFullStep.TabIndex = 3;
             this.radioButtonFullStep.Text = "Full Step";
             this.radioButtonFullStep.UseVisualStyleBackColor = true;
@@ -111,62 +163,21 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(17, 44);
+            this.label2.Location = new System.Drawing.Point(23, 54);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.Size = new System.Drawing.Size(96, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Microstepping";
             // 
-            // radioButtonOne8
-            // 
-            this.radioButtonOne8.AutoSize = true;
-            this.radioButtonOne8.Location = new System.Drawing.Point(129, 97);
-            this.radioButtonOne8.Name = "radioButtonOne8";
-            this.radioButtonOne8.Size = new System.Drawing.Size(65, 17);
-            this.radioButtonOne8.TabIndex = 5;
-            this.radioButtonOne8.Text = "1/8 step";
-            this.radioButtonOne8.UseVisualStyleBackColor = true;
-            // 
-            // radioButtonQuarterStep
-            // 
-            this.radioButtonQuarterStep.AutoSize = true;
-            this.radioButtonQuarterStep.Location = new System.Drawing.Point(129, 79);
-            this.radioButtonQuarterStep.Name = "radioButtonQuarterStep";
-            this.radioButtonQuarterStep.Size = new System.Drawing.Size(65, 17);
-            this.radioButtonQuarterStep.TabIndex = 6;
-            this.radioButtonQuarterStep.Text = "1/4 step";
-            this.radioButtonQuarterStep.UseVisualStyleBackColor = true;
-            // 
-            // radioButtonOne16
-            // 
-            this.radioButtonOne16.AutoSize = true;
-            this.radioButtonOne16.Location = new System.Drawing.Point(129, 115);
-            this.radioButtonOne16.Name = "radioButtonOne16";
-            this.radioButtonOne16.Size = new System.Drawing.Size(71, 17);
-            this.radioButtonOne16.TabIndex = 7;
-            this.radioButtonOne16.Text = "1/16 step";
-            this.radioButtonOne16.UseVisualStyleBackColor = true;
-            // 
-            // radioButtonOne32
-            // 
-            this.radioButtonOne32.AutoSize = true;
-            this.radioButtonOne32.Checked = true;
-            this.radioButtonOne32.Location = new System.Drawing.Point(129, 133);
-            this.radioButtonOne32.Name = "radioButtonOne32";
-            this.radioButtonOne32.Size = new System.Drawing.Size(71, 17);
-            this.radioButtonOne32.TabIndex = 8;
-            this.radioButtonOne32.TabStop = true;
-            this.radioButtonOne32.Text = "1/32 step";
-            this.radioButtonOne32.UseVisualStyleBackColor = true;
-            // 
             // MotorControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MotorControl";
-            this.Size = new System.Drawing.Size(240, 160);
+            this.Size = new System.Drawing.Size(320, 197);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSpeed)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
