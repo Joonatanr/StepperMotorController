aa8c910e5fb01d28b0dc00f91740e85502845b1c
Joonatan@work <joonatanrenel@gmail.com>
2018-10-04 16:19:58 +0300

    Added missing comma.

--- LOGIC/stepper.c
+++ LOGIC/stepper.c
@@ -68,7 +68,7 @@ Private const StepperConf_t priv_stepper_conf[NUMBER_OF_STEPPERS] =
 
        .m0_pin    = { GPIO_PORT_P1, GPIO_PIN7 },
        .m1_pin    = { GPIO_PORT_P5, GPIO_PIN0 },
-       .m2_pin    = { GPIO_PORT_P5, GPIO_PIN2 }
+       .m2_pin    = { GPIO_PORT_P5, GPIO_PIN2 },
 /*
        .m0_pin =    { GPIO_PORT_P3, GPIO_PIN7 },
        .m1_pin =    { GPIO_PORT_P3, GPIO_PIN5 },
